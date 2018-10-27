b6103e3c3725a2a059448af0f68eac348d1feb1d
Ryan Saunders <rsaunders@qualtrics.com>
2018-10-27 11:39:13 -0600

    added new key generator

--- store/src/store.js
+++ store/src/store.js
@@ -32,3 +32,44 @@ export const store = createStore(
     lazyReducerEnhancer(combineReducers),
     applyMiddleware(thunk))
 );
+
+export const keyFactory = {
+  containerIds: {},
+  storePreexistingKeys: (containerId, container) => {
+    _storeKeys(containerId, Object.keys(container))
+  },
+  getUniqueKey: (containerId) => {
+    this._initializeContainer(containerId)
+    let key = null
+    while (!this._isUnique(key)) {
+      key = '_' + Math.random().toString(36).substr(2, 9);
+    }
+    this._storeKey(containerId, key)
+    return key
+  },
+  removeKey: (containerId, key) => {
+    this._initializeContainer(containerId)
+    this.containerIds[containerId].remove(key)
+  },
+  clear: (containerId) => {
+    this._initializeContainer(containerId)
+    this.containerIds[containerId].clear()
+  },
+  _storeKeys: (containerId, keys) => {
+    this._initializeContainer(containerId)
+    for (key of keys) {
+      this._storeKey(containerId, key)
+    }
+  },
+  _storeKey: (containerId, key) => {
+    this.containerIds[containerId].add(key)
+  },
+  _isUnique: (containerId, key) => {
+    return !!key && this.containerIds[containerId].has(key)
+  },
+  _initializeContainer: (containerId) => {
+    if (!this.containerIds[containerId]) {
+      this.containerIds[containerId] = new Set()
+    }
+  }
+};
